0a938cbbd5818e21cd4d497dc5a6c71a1e626801
ashish53v <ashishjadhav53@gmail.com>
2017-07-07 12:42:04 -0400

    Check changes

--- scripts/app-new.js
+++ scripts/app-new.js
@@ -122,7 +122,7 @@ APP.Main = (function() {
       storyDetails.classList.add('story-details');
       storyDetails.innerHTML = storyDetailsHtml;
 
-      document.body.appendChild(storyDetails);
+      // document.body.appendChild(storyDetails);
 
       commentsElement = storyDetails.querySelector('.js-comments');
       storyHeader = storyDetails.querySelector('.js-header');
@@ -163,89 +163,97 @@ APP.Main = (function() {
 
   function showStory(id) {
 
-    if (inDetails)
-      return;
+    // if (inDetails)
+    //   return;
 
-    inDetails = true;
+    // inDetails = true;
 
-    var storyDetails = $('#sd-' + id);
-    var left = null;
+    // var storyDetails = $('#sd-' + id);
+    // var left = null;
 
-    if (!storyDetails)
-      return;
+    // if (!storyDetails)
+    //   return;
 
-    document.body.classList.add('details-active');
-    storyDetails.style.opacity = 1;
+    // document.body.classList.add('details-active');
+    // storyDetails.style.opacity = 1;
 
-    function animate () {
+    // function animate () {
 
-      // Find out where it currently is.
-      var storyDetailsPosition = storyDetails.getBoundingClientRect();
+    //   // Find out where it currently is.
+    //   var storyDetailsPosition = storyDetails.getBoundingClientRect();
 
-      // Set the left value if we don't have one already.
-      if (left === null)
-        left = storyDetailsPosition.left;
+    //   // Set the left value if we don't have one already.
+    //   if (left === null)
+    //     left = storyDetailsPosition.left;
 
-      // Now figure out where it needs to go.
-      left += (0 - storyDetailsPosition.left) * 0.1;
+    //   // Now figure out where it needs to go.
+    //   left += (0 - storyDetailsPosition.left) * 0.1;
 
-      // Set up the next bit of the animation if there is more to do.
-      if (Math.abs(left) > 0.5)
-        setTimeout(animate, 4);
-      else
-        left = 0;
+    //   // Set up the next bit of the animation if there is more to do.
+    //   if (Math.abs(left) > 0.5)
+    //     setTimeout(animate, 4);
+    //   else
+    //     left = 0;
 
-      // And update the styles. Wait, is this a read-write cycle?
-      // I hope I don't trigger a forced synchronous layout!
-      storyDetails.style.left = left + 'px';
-    }
+    //   // And update the styles. Wait, is this a read-write cycle?
+    //   // I hope I don't trigger a forced synchronous layout!
+    //   storyDetails.style.left = left + 'px';
+    // }
 
-    // We want slick, right, so let's do a setTimeout
-    // every few milliseconds. That's going to keep
-    // it all tight. Or maybe we're doing visual changes
-    // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    // // We want slick, right, so let's do a setTimeout
+    // // every few milliseconds. That's going to keep
+    // // it all tight. Or maybe we're doing visual changes
+    // // and they should be in a requestAnimationFrame
+    // setTimeout(animate, 4);
+
+    if(!storyDetails)
+        return;
+    storyDetails.classList.add('visible');
+    storyDetails.classList.remove('hidden');
   }
 
   function hideStory(id) {
 
-    if (!inDetails)
-      return;
+    // if (!inDetails)
+    //   return;
 
-    var storyDetails = $('#sd-' + id);
-    var left = 0;
+    // var storyDetails = $('#sd-' + id);
+    // var left = 0;
 
-    document.body.classList.remove('details-active');
-    storyDetails.style.opacity = 0;
+    // document.body.classList.remove('details-active');
+    // storyDetails.style.opacity = 0;
 
-    function animate () {
+    // function animate () {
 
-      // Find out where it currently is.
-      var mainPosition = main.getBoundingClientRect();
-      var storyDetailsPosition = storyDetails.getBoundingClientRect();
-      var target = mainPosition.width + 100;
+    //   // Find out where it currently is.
+    //   var mainPosition = main.getBoundingClientRect();
+    //   var storyDetailsPosition = storyDetails.getBoundingClientRect();
+    //   var target = mainPosition.width + 100;
 
-      // Now figure out where it needs to go.
-      left += (target - storyDetailsPosition.left) * 0.1;
+    //   // Now figure out where it needs to go.
+    //   left += (target - storyDetailsPosition.left) * 0.1;
 
-      // Set up the next bit of the animation if there is more to do.
-      if (Math.abs(left - target) > 0.5) {
-        setTimeout(animate, 4);
-      } else {
-        left = target;
-        inDetails = false;
-      }
+    //   // Set up the next bit of the animation if there is more to do.
+    //   if (Math.abs(left - target) > 0.5) {
+    //     setTimeout(animate, 4);
+    //   } else {
+    //     left = target;
+    //     inDetails = false;
+    //   }
 
-      // And update the styles. Wait, is this a read-write cycle?
-      // I hope I don't trigger a forced synchronous layout!
-      storyDetails.style.left = left + 'px';
-    }
+    //   // And update the styles. Wait, is this a read-write cycle?
+    //   // I hope I don't trigger a forced synchronous layout!
+    //   storyDetails.style.left = left + 'px';
+    // }
 
-    // We want slick, right, so let's do a setTimeout
-    // every few milliseconds. That's going to keep
-    // it all tight. Or maybe we're doing visual changes
-    // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    // // We want slick, right, so let's do a setTimeout
+    // // every few milliseconds. That's going to keep
+    // // it all tight. Or maybe we're doing visual changes
+    // // and they should be in a requestAnimationFrame
+    // setTimeout(animate, 4);
+
+    storyDetails.classList.add('hidden');
+    storyDetails.classList.remove('visible');
   }
 
   /**
